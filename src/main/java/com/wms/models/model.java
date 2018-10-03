ec84a34093a34a9a00a5952de09305249d87b127
254v0lkan <nzaro19@gmail.com>
2018-10-03 09:20:30 +0300

    Spring boot running, Hooray :-)

---
+++ src/main/java/com/wms/models/model.java
@@ -0,0 +1,5 @@
+package com.wms.models;
+
+public class model {
+
+}
---
+++ src/main/java/com/wms/waterMs/WaterMsApplication.java
@@ -0,0 +1,12 @@
+package com.wms.waterMs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterMsApplication.class, args);
+	}
+}
