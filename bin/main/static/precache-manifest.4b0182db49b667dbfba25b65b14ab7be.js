62e7fefbdea6cc85a69960318a610897c3dab167
volkan <16579273+mevolkan@users.noreply.github.com>
2022-11-15 00:08:41 +0300

    fixes

---
+++ bin/main/static/precache-manifest.4b0182db49b667dbfba25b65b14ab7be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "04bee49a49eeb62f8b89",
+    "url": "/static/js/main.04bee49a.chunk.js"
+  },
+  {
+    "revision": "472456c14af0cfc00d81",
+    "url": "/static/js/1.472456c1.chunk.js"
+  },
+  {
+    "revision": "04bee49a49eeb62f8b89",
+    "url": "/static/css/main.b0fee33d.chunk.css"
+  },
+  {
+    "revision": "472456c14af0cfc00d81",
+    "url": "/static/css/1.14be83d8.chunk.css"
+  },
+  {
+    "revision": "b0c9e4d939f6497a2534b967c91e9afc",
+    "url": "/index.html"
+  }
+];
